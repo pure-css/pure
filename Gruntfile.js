be5576df51aa22b168e90e5091960e535f29e6d6
Tilo Mitra <tilomitra@gmail.com>
2014-02-28 12:56:50 -0500

    update pure_grids with responsive sub-task

--- Gruntfile.js
+++ Gruntfile.js
@@ -199,6 +199,19 @@ grunt.initConfig({
             options: {
                 units: [5, 24]
             }
+        },
+
+        responsive: {
+            dest: 'build/grids-responsive.css',
+
+            options: {
+                mediaQueries: {
+                    sm: 'screen and (min-width: 35.5em)',   // 568px
+                    md: 'screen and (min-width: 48em)',     // 768px
+                    lg: 'screen and (min-width: 64em)',     // 1024px
+                    xl: 'screen and (min-width: 80em)'      // 1280px
+                }
+            }
         }
     },
 
