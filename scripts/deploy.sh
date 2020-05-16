2442a9e66b310df8ae1845812bc8066f96abee43
redonkulus <seth@bertalotto.net>
2020-05-16 11:21:10 -0700

    ci: add pure dir

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,6 +2,7 @@ git config --global user.name "${GH_NAME}"
 git config --global user.email "${GH_EMAIL}"
 echo "machine github.com login ${GH_NAME} password ${GH_TOKEN}" > ~/.netrc
 npm run prepare
+mkdir ./static/css/pure
 cp -rf ./build/ ./static/css/pure/
 cd site
 npm install
