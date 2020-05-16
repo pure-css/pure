2e49130c27019f454ee608560296ba349429f79e
redonkulus <seth@bertalotto.net>
2020-05-16 11:13:24 -0700

    ci: move copy to deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,7 @@
 git config --global user.name "${GH_NAME}"
 git config --global user.email "${GH_EMAIL}"
 echo "machine github.com login ${GH_NAME} password ${GH_TOKEN}" > ~/.netrc
+cp -rf ./build/ ./static/css/pure/
 cd site
 npm install
 GIT_USER="${GH_NAME}" npm run deploy
