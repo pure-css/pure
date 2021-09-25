36af5c7f2ddb76b1782c2f66a48117c2dd8e5ef0
redonkulus <seth@bertalotto.net>
2021-09-25 09:19:16 -0700

    site: add google verification meta

--- site/src/theme/Layout.js
+++ site/src/theme/Layout.js
@@ -81,6 +81,7 @@ function Layout(props) {
                     <meta property="og:url" content={siteUrl + permalink} />
                 )}
                 <meta name="twitter:card" content="summary_large_image" />
+                <meta name="google-site-verification" content="SOiZLX92OWf6JU_zcwEprxstgfUnSQwOpuTASyOv9tM" />
 
                 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway:200" />
             </Head>
