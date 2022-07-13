dabdfa4efd8b8b70e8aab537ca9a76223cc85173
DecliningLotus <declininglotus@gmail.com>
2022-07-14 01:46:10 +0300

    fix: add import for doc context

--- site/src/pages/base/index.js
+++ site/src/pages/base/index.js
@@ -1,4 +1,5 @@
 import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import React from 'react';
 import Layout from '../../theme/Layout';
 import Header from '../../../components/Header';
