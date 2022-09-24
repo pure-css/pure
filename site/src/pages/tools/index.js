0cce28360b7d1642266d853f05df54a6246732e9
ahron1 <85363537+ahron1@users.noreply.github.com>
2022-09-24 12:49:01 +0530

    Update index.js

--- site/src/pages/tools/index.js
+++ site/src/pages/tools/index.js
@@ -131,6 +131,7 @@ function Tools() {
                             lg: 'screen and (min-width: 64em)',   // 1024px
                             xl: 'screen and (min-width: 80em)',   // 1280px
                             xxl: 'screen and (min-width: 120em)'  // 1920px
+                            xxxl: 'screen and (min-width: 160em)'  // 2560px
                         }
                     })).toString();
 
